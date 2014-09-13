fca566aef6e8c7f56dbaffe8d4f612ac0336a738
Kawabata Shingo <nickel.app@gmail.com>
2014-09-13 15:23:49 +0900

    GetPhotoActivityギャラリー取得

--- HelloWorld/src/com/example/helloworld/activity/GetPhotoActivity.java
+++ HelloWorld/src/com/example/helloworld/activity/GetPhotoActivity.java
@@ -1,16 +1,41 @@
 package com.example.helloworld.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.example.helloworld.R;
+import com.example.helloworld.date.RequestCode;
 
-public class GetPhotoActivity extends Activity {
+public class GetPhotoActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_photo_get);
+
+		findViewById(R.id.button_start_gallery).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_start_gallery:
+			startGallery();
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	public void startGallery() {
+		Intent i = new Intent();
+		i.setAction(Intent.ACTION_GET_CONTENT);
+		i.setType("image/*");
+		startActivityForResult(i, RequestCode.REQUEST_GALLERY);
 	}
 
 }
---
+++ HelloWorld/src/com/example/helloworld/date/RequestCode.java
@@ -0,0 +1,11 @@
+package com.example.helloworld.date;
+
+public class RequestCode {
+
+	public static int REQUEST_GALLERY = 0;
+
+	private RequestCode() {
+
+	}
+
+}
